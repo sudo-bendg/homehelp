15084ad0e86b8f8226af5982ba1424fa2f2fa1ee
Benjamin Godfrey <ben.d.godfrey2002@gmail.com>
2025-03-24 21:26:43 +0000

    create Flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='./templates')
+
+@app.route('/')
+def home():
+    return render_template('files.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
