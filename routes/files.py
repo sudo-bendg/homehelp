b91c0d3471f8b48e6d85ee16a0aade871485a318
Benjamin Godfrey <ben.d.godfrey2002@gmail.com>
2025-03-28 16:31:47 +0000

    add file upload

--- routes/files.py
+++ routes/files.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, send_from_directory, abort
+from flask import Blueprint, render_template, send_from_directory, abort, request, redirect, url_for
 import os
+from werkzeug.utils import secure_filename
 
 workingDirectory = os.getcwd()
 currentRelPath = "/homehelpFiles"
@@ -38,3 +39,27 @@ def download_file(filename):
     if not os.path.isfile(file_path):
         abort(404)  # Return a 404 error if the file doesn't exist
     return send_from_directory(workingDirectory + currentRelPath, filename, as_attachment=True)
+
+@files_bp.route('/files/upload/<path:path>', methods=['POST'])
+def upload_file(path):
+    # Construct the full path to the current directory
+    fullPath = os.path.join(workingDirectory + currentRelPath, path)
+
+    # Ensure the path is valid and within the allowed directory
+    if not os.path.exists(fullPath) or not os.path.isdir(fullPath):
+        abort(404)  # Return a 404 error if the directory doesn't exist
+
+    # Check if a file was uploaded
+    if 'file' not in request.files:
+        return "No file part", 400
+
+    file = request.files['file']
+
+    # Check if the file has a valid name
+    if file.filename == '':
+        return "No selected file", 400
+
+    # Save the file to the current directory
+    filename = secure_filename(file.filename)  # Sanitize the filename
+    file.save(os.path.join(fullPath, filename))  # Save the file
+    return redirect(url_for('files.home', path=path))  # Redirect back to the current directory
