7b3cb38ffb7487feb12fc9985ba86c253bfd6831
Benjamin Godfrey <ben.d.godfrey2002@gmail.com>
2025-03-28 16:19:05 +0000

    file path reflected in url

--- routes/files.py
+++ routes/files.py
@@ -6,14 +6,30 @@ currentRelPath = "/homehelpFiles"
 
 files_bp = Blueprint('files', __name__)
 
-@files_bp.route('/')
-def home():
-    currentDirectoryFiles = os.listdir(workingDirectory + currentRelPath)
+@files_bp.route('/files/', defaults={'path': ''})
+@files_bp.route('/files/<path:path>')
+def home(path):
+    # Construct the full path to the requested directory
+    fullPath = os.path.join(workingDirectory + currentRelPath, path)
+
+    # Ensure the path is valid and within the allowed directory
+    if not os.path.exists(fullPath) or not os.path.isdir(fullPath):
+        abort(404)  # Return a 404 error if the directory doesn't exist
+
+    # List the contents of the directory
+    currentDirectoryFiles = os.listdir(fullPath)
     fileInfo = []
     for file in currentDirectoryFiles:
-        filePath = os.path.join(workingDirectory + currentRelPath + "/" + file)
+        filePath = os.path.join(fullPath, file)
         fileInfo.append({"name": file, "isDir": os.path.isdir(filePath)})
-    return render_template('files.html', files=sorted(fileInfo, key=lambda f: str(not f['isDir']) + f['name']))
+
+    # Sort the files: directories first, then files, both alphabetically
+    fileInfo = sorted(fileInfo, key=lambda f: (not f['isDir'], f['name'].lower()))
+
+    # Calculate the parent directory
+    parentPath = '/'.join(path.strip('/').split('/')[:-1]) if path else None
+
+    return render_template('files.html', files=fileInfo, currentPath=path, parentPath=parentPath)
 
 @files_bp.route('/download/<filename>')
 def download_file(filename):
